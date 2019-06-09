243242b5f88dd8a20d3f7c5c4c6f6b2d6ba135e7
Vykstorm <victorruizgomezdev@gmail.com>
2019-06-09 12:18:02 +0200

    Updated test case

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -18,8 +18,8 @@ class TestDecorators(TestCase):
 
     def test_wrapper_info(self):
         '''
-        Check that methods & attributes __name__ , __doc__ & __annotations__
-        are the same or return the same value for the wrapper & the wrapped functions
+        Check that methods & attributes __name__ , __doc__, __annotations__, __qualname__,
+        __str__ and __repr__ are the same or return the same value for the wrapper & the wrapped functions
         '''
 
         def foo(x :int, y : float, z : str) -> complex:
@@ -32,7 +32,9 @@ class TestDecorators(TestCase):
         self.assertEqual(foo.__name__, bar.__name__)
         self.assertEqual(foo.__doc__, bar.__doc__)
         self.assertEqual(foo.__annotations__, bar.__annotations__)
-
+        self.assertEqual(foo.__qualname__, bar.__qualname__)
+        self.assertEqual(str(foo), str(bar))
+        self.assertEqual(repr(foo), repr(bar))
 
     def test_wrapped_func_no_args(self):
         '''
@@ -65,7 +67,7 @@ class TestDecorators(TestCase):
 
     def test_wrapped_func_with_varargs(self):
         '''
-        Check that we can decorate a function with varadict position arguments (*args)
+        Check that we can decorate a function with varadict positional arguments (*args)
         Also verifies that values passed to the wrapped function are consistent with its signature
         '''
         @checked
@@ -153,6 +155,108 @@ class TestDecorators(TestCase):
         self.assertRaises(TypeError, qux)
         self.assertRaises(TypeError, baz)
 
+
+    def test_decorate_instance_method(self):
+        '''
+        Test that we can decorate intance methods
+        '''
+        tester = self
+        class Foo:
+            @checked
+            def bar(self, x):
+                tester.assertIsInstance(self, Foo)
+                tester.assertEqual(x, 1)
+
+        obj = Foo()
+
+        obj.bar(1)
+        Foo.bar(obj, 1)
+
+
+    def test_decorate_class_method(self):
+        '''
+        Can we decorate class methods ?
+        '''
+        tester = self
+        class Foo:
+            @classmethod
+            @checked
+            def bar(cls, x):
+                tester.assertEqual(cls, Foo)
+                tester.assertEqual(x, 1)
+
+        obj = Foo()
+        obj.bar(1)
+        Foo.bar(1)
+
+
+    def test_decorate_static_method(self):
+        '''
+        Can we decorate static methods ?
+        '''
+        tester = self
+        class Foo:
+            @staticmethod
+            @checked
+            def bar(x):
+                tester.assertEqual(x, 1)
+
+        obj = Foo()
+        obj.bar(1)
+        Foo.bar(1)
+
+
+    def test_decorate_property(self):
+        '''
+        Can we decorate properties ?
+        '''
+        tester = self
+        class Foo:
+            @property
+            @checked
+            def bar(self):
+                tester.assertIsInstance(self, Foo)
+                return 1
+
+            @property
+            def qux(self):
+                pass
+
+            @qux.setter
+            @checked
+            def qux(self, value):
+                tester.assertIsInstance(self, Foo)
+                tester.assertEqual(value, 2)
+
+        obj = Foo()
+        self.assertEqual(obj.bar, 1)
+        obj.qux = 2
+
+
+    def test_decorated_method_qualname(self):
+        '''
+        Methods will have __qualname__ attribute (the same as indicated in
+        the wrapped function)
+        '''
+
+        class Foo:
+            @checked
+            def bar(self):
+                pass
+
+            @classmethod
+            @checked
+            def qux(cls):
+                pass
+
+            @staticmethod
+            @checked
+            def baz():
+                pass
+
+        self.assertRegex(Foo.bar.__qualname__, 'Foo.bar$')
+        self.assertRegex(Foo.qux.__qualname__, 'Foo.qux$')
+        self.assertRegex(Foo.baz.__qualname__, 'Foo.baz$')
 
 
 if __name__ == '__main__':
