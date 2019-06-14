76f68853f1b6d408df0761429c84becaa24ed9d3
Vykstorm <victorruizgomezdev@gmail.com>
2019-06-14 17:14:54 +0200

    Reorganized code

--- wrappers.py
+++ wrappers.py
@@ -4,7 +4,8 @@ from inspect import signature
 from errors import ValidationError
 from itertools import count
 from functools import lru_cache, partial, update_wrapper
-from inspect import signature, Parameter, Signature
+import inspect
+from inspect import Parameter, Signature
 from parser import parse_annotation
 import types
 
@@ -50,80 +51,158 @@ class CallableWrapper:
 
 
 
-class SignatureMixin:
+class ValidateFuncWrapper(CallableWrapper):
     '''
-    Helper class that provides additional helper methods over inspect.Signature
-    class to parse function annotations to validators (used to build wrappers
-    around decorated functions)
+    An object of this class is a wrapper to a given function but it incorporates
+    validation features, such as checking the type of the input arguments or the return
+    value
     '''
-    def __init__(self, sig):
-        self.sig = sig
+    def __init__(self, func, options):
+        '''
+        Initializes this instance.
+        :param func: Is the function to be wrapped
+        :param options: Must be a dictionary with settings (check the module config)
+        '''
+        super().__init__(func)
+        self.options = options
 
-    def __getattr__(self, key):
-        return getattr(self.sig, key)
+        # No varadic keyword parameter annotations allowed
+        if self.varkwargs_param is not None and self.varkwargs_param.annotation is not Signature.empty:
+            raise ValueError('Varadic keyword parameter annotations are not allowed')
+
+
+    @property
+    @lru_cache(maxsize=1)
+    def signature(self):
+        '''
+        Returns the signature of the wrapped function
+        '''
+        return inspect.signature(self)
+
 
     @property
     @lru_cache(maxsize=1)
     def varargs_param(self):
-        # Return the parameter in the signature which is of type *args
-        # or None if *args is not in the signature
-        for param in self.parameters.values():
+        '''
+        Return the parameter (instance of class inspect.Parameter)
+        in the signature of the wrapped function which is of type *args
+        or None if *args is not in the signature
+        '''
+        for param in self.signature.parameters.values():
             if param.kind == Parameter.VAR_POSITIONAL:
                 return param
         return None
 
+
     @property
     @lru_cache(maxsize=1)
     def varkwargs_param(self):
-        # Return the parameter in the signature which is of type **kwargs
-        # or None if **kwargs is not in the signature
-        for param in self.parameters.values():
+        '''
+        Return the parameter (instance of class inspect.Parameter)
+        in the signature of the wrapped function which is of type **kwargs
+        or None if **kwargs is not in the signature
+        '''
+        for param in self.signature.parameters.values():
             if param.kind == Parameter.VAR_KEYWORD:
                 return param
         return None
 
 
-    def get_validators(self, options):
+    @property
+    @lru_cache(maxsize=1)
+    def param_validators(self):
         '''
-        Parse signature annotations to validators
-        Return a tuple of two items:
-        - A dictionary that maps parameter names to validators
-        - Validator obtained parsing function return annotation
+        Returns a list of validators (on for each parameter in the wrapped function signature),
+        that will be used to validate the input arguments
         '''
+        signature = self.signature
+        validators = []
 
-        # Get param validators
-        def get_param_validators():
-            for param in self.parameters.values():
-                yield parse_annotation(
-                    Any if param.annotation is Parameter.empty else param.annotation,
-                    options
-                )
-        param_validators = dict(zip(self.parameters.keys(), get_param_validators()))
+        for param in signature.parameters.values():
+            validators.append(parse_annotation(
+                Any if param.annotation is Parameter.empty else param.annotation,
+                self.options
+            ))
+        return dict(zip(signature.parameters.keys(), validators))
 
-        # Get return validator
-        ret_validator = parse_annotation(
-                Any if self.return_annotation is Parameter.empty else self.return_annotation,
-                options
+
+    @property
+    @lru_cache(maxsize=1)
+    def return_validator(self):
+        '''
+        Returns the validator that will be used to validate the return value
+        '''
+        signature = self.signature
+        return parse_annotation(
+                Any if signature.return_annotation is Parameter.empty else signature.return_annotation,
+                self.options
             )
 
-        return param_validators, ret_validator
+
+    def bind(self, *args, **kwargs):
+        '''
+        Binds the given arguments to the signature of the wrapped function
+        (also applies default values)
+        '''
+        bounded = self.signature.bind(*args, **kwargs)
+        bounded.apply_defaults()
+        return bounded
+
+
+    def validate_input(self, *args, **kwargs):
+        '''
+        Validates the given input arguments.
+        Returns a tuple with the values args and kwargs which are the positional and
+        keyword arguments that should be passed when calling to the wrapped function
+        '''
+        if not self.options['match_args']:
+            # Dont validate args
+            return args, kwargs
+
+        bounded = self.bind(*args, **kwargs)
+
+        args = []
+        varargs = []
+        varkwargs = bounded.kwargs
+
+        for key, value in bounded.arguments.items():
+            param = self.signature.parameters[key]
+
+            if param.kind == Parameter.VAR_KEYWORD:
+                # **kwargs
+                continue
+            if param.kind == Parameter.VAR_POSITIONAL:
+                # *args
+                if key in self.param_validators and self.options['match_varargs']:
+                    validate = partial(self.param_validators[key].validate, context={'func': self.__name__, 'param': 'items on *{}'.format(key)})
+                    varargs.extend(map(validate, value))
+                else:
+                    # *args items will not be validated
+                    varargs.extend(value)
+            else:
+                # Regular argument
+                validate = partial(self.param_validators[key].validate, context={'func': self.__name__, 'param': key})
+                args.append(validate(value))
+
+        args += varargs
+        return args, kwargs
 
 
 
-class ValidateFuncWrapper(CallableWrapper):
-    def __init__(self, func, options):
-        super().__init__(func)
-        self.options = options
+    def validate_output(self, output):
+        '''
+        Validates the return value (the output of the wrapped function).
+        The value returned by this method will be used as the output of the call to
+        this wrapper function
+        '''
+        if not self.options['match_return']:
+            # Dont validate output
+            return output
 
-        # Get function signature
-        self.signature = SignatureMixin(signature(self))
+        return self.return_validator.validate(
+            output,
+            context={'func': self.__name__, 'param': 'return value'})
 
-        # No varadic keyword parameter annotations allowed
-        if self.signature.varkwargs_param is not None and self.signature.varkwargs_param.annotation is not Signature.empty:
-            raise ValueError('Varadic keyword parameter annotations are not allowed')
-
-        # Turn annotations into validators
-        self.param_validators, self.return_validator = self.signature.get_validators(self.options)
 
 
     def __get__(self, obj, objtype):
@@ -133,51 +212,7 @@ class ValidateFuncWrapper(CallableWrapper):
 
 
     def __call__(self, *args, **kwargs):
-        '''
-        This method is called when the user invokes the wrapped function
-        '''
-        # Bind arguments like if we call to the wrapped function
-        bounded = self.signature.bind(*args, **kwargs)
-
-        # Apply function default values
-        bounded.apply_defaults()
-
-        # Validate each argument
-        if self.options['match_args']:
-            args = []
-            varargs = []
-            varkwargs = bounded.kwargs
-
-            for key, value in bounded.arguments.items():
-                param = self.signature.parameters[key]
-
-                if param.kind == Parameter.VAR_KEYWORD:
-                    # **kwargs
-                    continue
-                if param.kind == Parameter.VAR_POSITIONAL:
-                    # *args
-                    if key in self.param_validators and self.options['match_varargs']:
-                        validate = partial(self.param_validators[key].validate, context={'func': self.__name__, 'param': 'items on *{}'.format(key)})
-                        varargs.extend(map(validate, value))
-                    else:
-                        # *args items will not be validated
-                        varargs.extend(value)
-                else:
-                    # Regular argument
-                    validate = partial(self.param_validators[key].validate, context={'func': self.__name__, 'param': key})
-                    args.append(validate(value))
-            args += varargs
-
-        else:
-            # No validation is performed on input arguments
-            args = bounded.args
-
-        # Now call the wrapped function
+        args, kwargs = self.validate_input(*args, **kwargs)
         result = super().__call__(*args, **kwargs)
-
-        if self.options['match_return']:
-            # Validate the return value
-            result = self.return_validator.validate(result, context={'func': self.__name__, 'param': 'return value'})
-
-        # Return the final output
+        result = self.validate_output(result)
         return result
