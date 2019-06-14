d0fd7de57fd331e21868a05b7d1e373fbcd73050
Vykstorm <victorruizgomezdev@gmail.com>
2019-06-14 15:38:43 +0200

    Added class CallableWrapper

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,6 +3,8 @@
 Helper functions for general purpose on this library
 '''
 
+from functools import update_wrapper
+
 
 def ordinal(k):
     '''
@@ -16,3 +18,34 @@ def ordinal(k):
     if k == 2:
         return '2nd'
     return '{}th'.format(k)
+
+
+
+
+class CallableWrapper:
+    '''
+    Objects of this class wraps callable objects:
+    - Calling this instance will invoke the wrapped callable with the same arguments.
+    - Also it has the attributes __name__, __qualname__, __module__, __annotations__,
+    __doc__ equal than the wrapped callable.
+    - _str__ & __repr__ returns the same as if you call __str__ and __repr__ in the wrapped object
+    '''
+    def __init__(self, obj):
+        '''
+        Initializes this instance.
+        :param obj: Is the object to be wrapped. Must be callable
+        '''
+        assert callable(obj)
+        self.obj = obj
+        update_wrapper(self, obj)
+
+
+    def __call__(self, *args, **kwargs):
+        return self.obj(*args, **kwargs)
+
+
+    def __str__(self):
+        return str(self.obj)
+
+    def __repr__(self):
+        return repr(self.obj)
